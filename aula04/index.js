0fbd6c800814db5c0687bd559e3d3731180c6ca8
Sants16 <jvsantosrocha980@gmail.com>
2022-09-21 16:02:37 -0300

    params/nodemon

--- aula04/index.js
+++ aula04/index.js
@@ -5,13 +5,24 @@ expressApp.get('/', (requisição, resposta) => {
     resposta.send('Hello Express.js')
 })
 
-expressApp.get('/info', (requisição, resposta) => {
+expressApp.get('/sobre', (requisição, resposta) => {
     resposta.send('Utilizando Express.js pela primeira vez')
 })
 
 expressApp.get('/data', (requisição, resposta) => {
     const data = new Date
     resposta.send(`Agora são ${data.getHours()}:${data.getMinutes()} do dia ${data.getDay()} do ano de ${data.getFullYear()}`)
+})
+
+expressApp.get('/saudacao/:nome/:idade/:nacionalidade', (requisição, resposta) => { //trabalhando com parâmetro, "/:nome do parametro"
+    const { nome, idade, nacionalidade } = requisição.params //pegando os parametros da requisição feita para o servidor node
+    resposta.send(`<h1>
+                        Olá
+                        <span style="color:blue;">${nome}</span>, 
+                        você tem 
+                        <span style="color:blue;">${idade}</span> anos 
+                        e é <span style="color:blue;">${nacionalidade}</span>
+                    </h1>`)
 })
 
 //tem que ser a ultima linha do codigo
