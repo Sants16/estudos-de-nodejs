05342aabd05fc90ea90665e5c2cf82ba56545e25
Sants16 <jvsantosrocha980@gmail.com>
2022-10-14 15:13:41 -0300

    validacoes-e-exibicoes

--- blogapp/app.js
+++ blogapp/app.js
@@ -1,39 +1,66 @@
-// Carregando módulos
+//* Carregando módulos
     const express = require('express')
     const handlebars = require('express-handlebars')
     const bodyParser = require('body-parser')
     const app = express()
-    const admin = require('./routes/admin') //arquivo admin.js na pasta de routes
-    //const mongoose = require('mongoose')
+    const admin = require('./routes/admin') //? arquivo admin.js na pasta de routes
     const path = require('path')
-const { default: mongoose } = require('mongoose')
+    const mongoose = require('mongoose')
+    const session = require('express-session')
+    const flash = require('connect-flash')
+    const moment = require('moment') //? modulo para melhorar a visualização da data
 
-// Configurações
-    // Body Parser
+//* Configurações
+    //? Sessão
+        app.use(session({
+            secret: 'cursodenode',
+            resave: true,
+            saveUninitialized: true
+        }))
+        //? Flash
+        app.use(flash())
+
+    //? Middleware
+     app.use((req, resp, next) => {
+        //* Assim criamos variáveis globais
+        resp.locals.success_msg = req.flash('success_msg')
+        resp.locals.error_msg = req.flash('error_msg')
+
+        next() //! Caso não coloquemos o next() o middleware vai parar a nossa requisição e a página vai ficar carregando infinitamente
+    })
+
+    //? Body Parser
         app.use(bodyParser.urlencoded({extended: true}))
         app.use(bodyParser.json())
 
-    // Handlebars
-        app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
+    //? Handlebars
+        app.engine('handlebars', handlebars.engine({
+            defaultLayout: 'main',
+            helpers: {
+                formatDate: (date) => {
+                     return moment(date).format('DD/MM/YYYY')
+                 }
+            }      
+        }))
         app.set('view engine', 'handlebars')
 
-    // Mongoose
-    try{
-        mongoose.connect('mongodb://localhost/blogapp', { useNewUrlParser: true })
-        .then(() => {
-            console.log('Conectado ao banco de dados')
-        })
-    } catch(err) {
-        console.log(err)
-    }
+    //? Mongoose
+        try{
+            mongoose.connect('mongodb://localhost/blogapp', { useNewUrlParser: true })
+            .then(() => {
+                console.log('Conectado ao banco de dados')
+            })
+        } catch(err) {
+            console.log(err)
+        }
 
-    // Pasta Public, pasta de arquivos estáticos
+    //? Pasta Public, pasta de arquivos estáticos
         app.use(express.static(path.join(__dirname + '/public')))
 
-// Rotas
-    app.use('/admin', admin) //todas as rotas do arquivo admin.js terão de ter o "prefixo de rota" /admin, ou seja, /admin/posts, /admin/categorias e etc
+//* Rotas
+    app.use('/admin', admin) //? todas as rotas do arquivo admin.js terão de ter o "prefixo de rota" /admin, ou seja, /admin/posts, /admin/categorias e etc
 
 const PORT = 8081
 app.listen(PORT, () => {
-    console.log('Servidor rodando, localhost:8081');
+    console.log('Servidor rodando, http://localhost:8081');
 })
--- blogapp/routes/admin.js
+++ blogapp/routes/admin.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const router = express.Router() //serve para criar rodas em arquivos separados
+const router = express.Router() //serve para criar rotas em arquivos separados
 
 // Usando model de forma externa com mongoose
 const mongoose = require('mongoose')
@@ -15,7 +15,13 @@ router.get('/posts', (req, resp) => {
 })
 
 router.get('/categorias', (req, resp) => {
-    resp.render('admin/categorias')
+    Categoria.find().sort({data: 'desc'}).lean()
+    .then((categorias) => {
+        resp.render('admin/categorias', {categorias})
+    }).catch(err => {
+        req.flash('error_msg', 'Houve um erro ao listar as categorias')
+        req.redirect('/admin')
+    })
 })
 
 router.get('/categorias/add', (req, resp) => {
@@ -25,14 +31,41 @@ router.get('/categorias/add', (req, resp) => {
 router.post('/categorias/nova', (req, resp) => {
     const { nome, slug } = req.body
 
-    const novaCategoria = {
-        nome, //nome: nome
-        slug //slug: slug
+    //* Validação
+    let erros = []
+
+    if(!nome || typeof nome === undefined || nome === null){
+        erros.push({
+            texto: 'Nome inválido'
+        })
     }
 
+    if(!slug || typeof slug === undefined || slug === null){
+        erros.push({
+            texto: 'Slug inválido'
+        })
+    }
+
+    if(erros.length > 0){
+       resp.render('admin/addcategorias', {
+        erros //erros: erros
+       }) 
+    } else {
+        const novaCategoria = {
+            nome, //nome: nome
+            slug //slug: slug
+        }
+    
         new Categoria(novaCategoria).save()
-        .then(() => console.log('Categoria foi salva com sucesso'))
-        .catch(err => console.log(err))
+        .then(() => {
+            req.flash('success_msg', 'Categoria criada com sucesso')
+            resp.redirect('/admin/categorias')
+        })
+        .catch(err => {
+            req.flash('error_msg', 'Houve um erro ao salvar a categoria, tente novamente')
+            resp.redirect('/admin')
+        })
+    }
 })
 
 module.exports = router
